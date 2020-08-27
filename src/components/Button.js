5d1d3b2294d0afcf1e1395af034e44981a9edee0
Maciej Perzankowski <perzankowski.m2001@gmail.com>
2020-08-27 16:05:18 +0200

    header section && button section && recipe section

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/Button.css";
+
+const Button = () => {
+  return (
+    <div className="button">
+      <button className="buttonAddRecipe"><span>Dodaj</span> Przepis</button>
+    </div>
+  )
+}
+
+export default Button;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/Header.css"
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="header__heading"><span className="header__heading--special">Keto</span>Przepis</h1>
+    </div>
+  )
+}
+
+export default Header;
